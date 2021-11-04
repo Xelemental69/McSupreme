693ae2123ebf30e9ab972e9f427f4ede3adedc44
Xelemental <xelemental864@gmail.com>
2021-11-04 10:43:43 +0100

    Implementada una forma de delay (y salsa)

--- 3deNoviembreFJMG/src/main/java/RuletaMacetas/Bo5_1v1_SinCamiseta.java
+++ 3deNoviembreFJMG/src/main/java/RuletaMacetas/Bo5_1v1_SinCamiseta.java
@@ -16,24 +16,79 @@ public class Bo5_1v1_SinCamiseta {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        //TimeUnit tu=TimeUnit.SECONDS;
-
-        System.out.println("Bienvenidos a la RULETA DE MACETÓPOLIS");
-        
-        //tu.SECONDS.sleep(1);
-        
-        System.out.println("HOY, LA SUERTE DECIDIRÄ QUIÉN DE NUESTRAS "
-        + "VÍCTIMAS; perdón, JUGADORES TENDRÁ QUE INVITAR A VICO A UN HELADO");
         
         // Declaramos nuestras variables:
         Random ruleta = new Random();
         Random rng = new Random();
         int j1Score = 0,j2Score = 0,numj1,numj2,nT;
+        int ronda = 1;
+        
+
+        System.out.println("Bienvenidos a la RULETA DE MACETÓPOLIS");
+        
+        try {//Testeando una forma de pausar el programa por 1 segundo...            
+        TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ie) {
+        Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("HOY, LA SUERTE DECIDIRÁ QUIÉN DE NUESTRAS "
+        + "VÍCTIMAS...");
+        
+        try {//Testeando una forma de pausar el programa por 1 segundo...            
+        TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ie) {
+        Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("perdón...\n JUGADORES TENDRÁ QUE INVITAR A "
+                + "VICO A UN HELADO");
+        
+        try {//Testeando una forma de pausar el programa por 1 segundo...            
+        TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ie) {
+        Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("Comenzamos en 3...");
+        
+        try {//Testeando una forma de pausar el programa por 1 segundo...            
+        TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ie) {
+        Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("...2...");
+        
+        try {//Testeando una forma de pausar el programa por 1 segundo...            
+        TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ie) {
+        Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("...1...");
+        
+        try {//Testeando una forma de pausar el programa por 1 segundo...            
+        TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ie) {
+        Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("LET'S FUCKING GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!");
+        
+        
         
         //Iniciamos el bucle para el mejor de 5
         while(j1Score < 3 && j2Score < 3){
+            
+            try {//Testeando una forma de pausar el programa por 2 segundos...            
+        TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException ie) {
+        Thread.currentThread().interrupt();
+        }
+            
+            //Mostramos el nº de ronda
+            System.out.println(ronda + "a ronda: ");
             
             //Generamos los números al azar y los mostramos:
             numj1 = rng.nextInt(5) + 1;
@@ -48,14 +103,19 @@ public class Bo5_1v1_SinCamiseta {
             //Si es mayor a 7, J1 gana, si es menor a 7, J2 gana.
            //Y si es 7, nadie gana
             if(nT>7){
-                System.out.println("El jugador 1 gana esta ronda");
+                System.out.println("El jugador 1 gana esta ronda"
+                + "\nJugador 2, GIT GUD.");
                 j1Score++;
             }else if(nT<7){
-                System.out.println("El jugador 2 gana esta ronda");
+                System.out.println("El jugador 2 gana esta ronda"
+                + "\nJugador 1, GIT GUD.");
                 j2Score++;
             }else{
-                System.out.println("Empate, nadie gana puntos");
+                System.out.println("Empate, nadie gana puntos."
+                + "\nVenga, con más ánimo que vuestro profesor tiene hambre.");
             }
+            
+            ronda++;
             
         }
         
@@ -63,9 +123,13 @@ public class Bo5_1v1_SinCamiseta {
         System.out.println("Y el ganador de esta partida es...");
         
         if(j1Score == 3){
-            System.out.println("El Jugador 1");
+            System.out.println("El Jugador 1"
+            + "\n¡Felicidades Jugador 2,\n ha ganado el honor de invitar a Vico a "
+                    + " un helado!");
         }else if (j2Score == 3){
-            System.out.println("El Jugador 2");
+            System.out.println("El Jugador 2"
+            + "\n¡Felicidades Jugador 1,\n Ha conseguido el honor de invitar a Vico"
+                    + " a un helado!");
         }
         
     }
