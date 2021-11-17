761d6ed10fd0c30e5586aa1cb9d0011b4e2ecea6
Xelemental <xelemental864@gmail.com>
2021-11-17 14:43:58 +0100

    Nuevo Commit

---
+++ Contrasenias/src/main/java/daw/clavesfjmg/contrasenias/GenerarClave.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw.clavesfjmg.contrasenias;
+import java.util.*;
+/**
+ *
+ * @author fco-j
+ */
+public class GenerarClave {
+    public static void main(String[] args) {
+        
+        Random candado = new Random();
+        int v, w, x, y;
+        char z;
+        
+        do{
+            //Generamos los números dentro de un bucle en el cual solo podrán
+           //salir si la suma de ambos es impar
+            v = candado.nextInt(10);
+            y = candado.nextInt(10);
+            
+        }while( (v + y) % 2 == 0);
+        
+        do{
+            //Generamos los números dentro de un bucle en el cual solo podrán
+           //salir si el producto de ambos es par
+            w = candado.nextInt(10);
+            x = candado.nextInt(10);
+            
+        }while( (w * x) % 2 != 0);
+        
+        //Generamos una letra
+        z = (char) (candado.nextInt(122 - 33 + 1) + 33);
+        
+        System.out.println("Su clave generara es: " + v + w + x + y + z);
+        
+    }    
+    
+}
---
+++ Contrasenias/src/main/java/sauce/OhJoderNoQueHabeisHecho.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sauce;
+
+import java.util.*;
+
+/**
+ *
+ * @author fco-j
+ */
+public class OhJoderNoQueHabeisHecho {
+    
+    public static void main(String[] args) {
+        //Generamos las variables para hacer una clave potencialmente compleja
+        Random candado = new Random();
+        int v, w, x, y;
+        char z, a, b, c;
+        int order;
+        
+        do{
+            //Generamos los números dentro de un bucle en el cual solo podrán
+           //salir si la suma de ambos es impar
+            v = candado.nextInt(10);
+            y = candado.nextInt(10);
+            
+        }while( (v + y) % 2 == 0);
+        
+        do{
+            //Generamos los números dentro de un bucle en el cual solo podrán
+           //salir si el producto de ambos es par
+            w = candado.nextInt(10);
+            x = candado.nextInt(10);
+            
+        }while( (w * x) % 2 != 0);
+        
+        //Generamos una letra
+        z = (char) (candado.nextInt(122 - 33 + 1) + 33);
+        
+        do{
+            //Generamos otra letra dentro de un bucle que dejará salir un valor
+           //que no sea idéntico al anterior
+            a = (char) (candado.nextInt(122 - 33 + 1) + 33);
+        }while(a==z);
+        
+        do{
+            //Generamos otra letra dentro de un bucle que dejará salir un valor
+           //que no sea idéntico a los anteriores
+            b = (char) (candado.nextInt(122 - 33 + 1) + 33);
+        }while(b == z && b == a);
+        
+        //Generamos la última letra
+        c = (char) (candado.nextInt(122 - 33 + 1) + 33);
+            
+        //Generamos un nº al azar que determinará el orden de los caracteres
+        order = candado.nextInt(4);
+            
+        switch(order){//Según order, el orden de los carateres será distinto.
+            case 0:
+                System.out.println("1ª variación:");
+                System.out.println("Su clave generara es: " + v + a + x
+                    + b + w + z + y + c);
+                break;
+            case 1:
+                System.out.println("2ª variación:");
+                System.out.println("Su clave generara es: " + c + w + y
+                    + z + a + b + x + v);
+                break;
+                
+            case 2:
+                System.out.println("3ª variación:");
+                System.out.println("Su clave generara es: " + z + a + v
+                    + x + w + c + y + b);
+                break;
+                
+            case 3:
+                System.out.println("4ª variación:");
+                System.out.println("Su clave generara es: " + w + a + z
+                    + y + c + x + v + b);
+                break;
+        }   
+    }
+    
+}
