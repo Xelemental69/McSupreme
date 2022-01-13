64a2fc1fdaac665efc6a10c7f0a76deb3fd1c1b9
Xelemental <xelemental864@gmail.com>
2022-01-13 11:07:41 +0100

    Clase y prueba de nºs complejos y sus operaciones

---
+++ ClasesT4/src/main/java/daw1st_fjmg/clasest4/Complejo.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw1st_fjmg.clasest4;
+
+/**
+ *
+ * @author fco-j
+ */
+public class Complejo {
+
+    private double a; //Parte real del nº complejo (z)
+    private double b; //Parte imaginaria del nº complejo (z)
+
+    public Complejo(double a, double b) {
+        //Constructor que recoge los parametros de a y b para darles su respectivo
+        //valor:
+        this.a = a;
+        this.b = b;
+        
+    }
+
+    public Complejo() {
+        //Constructor por defecto que pone los valores en 0
+        a = 0;
+        b = 0;
+
+    }
+
+    public double getA() {//Método que permite modificar la parte real de z
+        
+        return a;
+        
+    }
+
+    public void setA(double a) {
+        
+        this.a = a;
+        
+    }
+
+    public double getB() {//Método que devuelve la parte imaginaria de z
+        
+        return b;
+        
+    }
+
+    public void setB(double b) {//Método que permite modificar
+        //la parte imaginaria de z
+        this.b = b;
+        
+    }
+    
+    public void suma(double x, double y){
+        //Método que suma dos nºs imaginarios...
+        a = a + x; //Sumándose las partes reales
+        b = b + y; //Y sumándose las partes imaginarias
+        
+    } 
+    
+    public void resta(double x, double y){
+        //Método que resta dos nºs imaginarios...
+        a = a - x; //Restándose las partes reales
+        b = b - y; //Y restándose las partes imaginarias
+        
+    } 
+    
+    public void multiplicacion(double x, double y){
+        //Método que multiplica dos nºs imaginarios...
+        double a2, b2;
+        a2 = a * x - b * y; //Así sería la operación en la parte real
+        //(se le resta a la multiplicación de las partes reales la de las
+        //imaginarias)
+        
+        b2 = b * x + y * a; //Y así sería en la parte imaginaria
+        //(se suman los productos de una parte real con la parte imaginaria del
+        //otro nº)
+        
+        a = a2;
+        b = b2;
+        
+    } 
+    
+    public void division(double x, double y){
+        //Método que multiplica dos nºs imaginarios...
+        double a2, b2,
+               xsqr = x * x,
+               ysqr = y * y;
+        a2 = (a * x + b * y) / (xsqr + ysqr); 
+        //Así sería la operación en la parte real (se le suma a la 
+        //multiplicación de las partes reales la de las
+        //imaginarias y se divide entre la suma de los cuadrados de las partes
+        //del nº introducido)
+        
+        b2 = (b * x - y * a) / (xsqr + ysqr); //Y así sería en la parte imaginaria
+        //(se restan los productos de una parte real con la parte imaginaria del
+        //otro nº y se divide entre la suma de los cuadrados de las partes del
+        //nº introducido))
+        
+        a = a2;
+        b = b2;
+        
+    }
+    
+    public void igualdad(double x, double y){
+        
+        if(a == x && b == y){
+            System.out.println("Los números son iguales");
+        }else{
+            System.out.println("Los números no son iguales");
+        }
+        
+        if(x<a){
+            System.out.println("La parte real de la clase es menor a"
+                    + " la introducida");
+        }        
+        if(x>a){
+            System.out.println("La parte real de la clase es mayor a"
+                    + " la introducida");
+        }
+        
+        if(y<b){
+            System.out.println("La parte imaginaria de la clase es menor a"
+                    + " la introducida");
+        }        
+        if(y>b){
+            System.out.println("La parte imaginaria de la clase es mayor a"
+                    + " la introducida");
+        }
+        
+    }
+
+    @Override
+    public String toString() {//Método que devuelve z
+        
+        return "Complejo{\n" + "z = " + a + " + " + b + "i\n" + '}';
+        
+    }
+
+}
---
+++ ClasesT4/src/main/java/daw1st_fjmg/clasest4/LoComplejo.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package daw1st_fjmg.clasest4;
+
+/**
+ *
+ * @author fco-j
+ */
+public class LoComplejo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Declaramos los números imaginarios "complejito1" y "complejito2":
+        Complejo complejito1 = new Complejo(); //Construido sin parámetros
+        Complejo complejito2 = new Complejo(6,9); //Construido con parámetros
+        
+        System.out.println(complejito1); //Muestra los valores por defecto
+        System.out.println("");
+        
+        //Se le asigna valores a complejito1:
+        complejito1.setA(8);
+        complejito1.setB(4);
+        
+        //Se muestran los valores de ambos complejitos:
+        System.out.println(complejito1);
+        System.out.println(complejito2);
+        System.out.println("");
+        
+        //Se crea un nuevo nº complejo ("complejito12"), que usaremos para 
+        //realizar operaciones:
+        Complejo complejito12 = new Complejo(complejito1.getA(),complejito1.getB());
+        
+        //Se muestra el valor original de complejito12:        
+        System.out.println(complejito12);
+        
+        //A continuación, se realizan las siguientes operaciones, junto a sus
+        //resultados:
+        
+        //Suma:
+        complejito12.suma(complejito2.getA(), complejito2.getB());
+        System.out.println(complejito12);
+        //Multiplicación:
+        complejito12.multiplicacion(complejito2.getA(), complejito2.getB());
+        System.out.println(complejito12);
+        //Resta:
+        complejito12.resta(complejito2.getA(), complejito2.getB());
+        System.out.println(complejito12);
+        //División:
+        complejito12.division(complejito2.getA(), complejito2.getB());
+        System.out.println(complejito12);
+        
+    }
+    
+}
