7d60da394fc849f140a4c97bfb5e45435b3fe678
Xelemental <xelemental864@gmail.com>
2022-01-20 12:46:55 +0100

    Ejercicios de arrays (por terminar)

---
+++ ArraysT5/src/main/java/ejerciciosDejaVu/Ej_2.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciosDejaVu;
+
+/**
+ *
+ * @author fco-j
+ */
+
+import java.util.*;
+
+public class Ej_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Nota: Acuérdate de que el error de cuando se sale del 
+        //rango del array es ArrayIndexOutofBounds :)
+        
+        int limite;
+        Scanner entry = new Scanner(System.in);
+        
+        System.out.println("Introduce el nº de personas");
+        limite = entry.nextInt();
+        
+        double[] alturas = new double[limite];
+        
+    }
+    
+}
