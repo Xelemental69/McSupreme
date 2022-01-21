92732cc65ac79eb20a4b11c63f6aa55c2dcd9ae5
Xelemental <xelemental864@gmail.com>
2022-01-21 13:54:42 +0100

    Ejercicio 2 de arrays terminado

--- ArraysT5/src/main/java/ejerciciosDejaVu/Ej_2.java
+++ ArraysT5/src/main/java/ejerciciosDejaVu/Ej_2.java
@@ -21,13 +21,102 @@ public class Ej_2 {
         // Nota: Acuérdate de que el error de cuando se sale del 
         //rango del array es ArrayIndexOutofBounds :)
         
-        int limite;
+        //Declaramos las variables (y el retorno del Scanner)
+        int limite, total, media, inf, sup;
         Scanner entry = new Scanner(System.in);
         
-        System.out.println("Introduce el nº de personas");
-        limite = entry.nextInt();
-        
-        double[] alturas = new double[limite];
+        do{//Preparamos un bucle en caso de que no se introduzcan bien los datos 
+            
+            try{//Preparamos un try catch por si introducen caracteres no 
+                //numéricos
+            
+                //Pedimos el nº de personas por teclado
+                System.out.println("Introduce el nº de personas");
+                limite = entry.nextInt();
+                //Inicializamos las variables que usaremos en futuros bucles:
+                total = 0;
+                inf = 0;
+                sup = 0;
+                        
+                if(limite > 0){//Si el nº de personas es introducido bien...   
+                
+                    //Ponemos como rango del array el int que hemos pedido 
+                    //por pantalla (límite):
+                    int[] alturas = new int[limite];
+                    int i;//Declaramos i aquí para poder usarlo fuera del bucle.
+               
+                    for(i = 0; i < limite; i++){//Bucle en el que pedimos la 
+                        //altura de cada persona por teclado
+                   
+                        System.out.println("Introduce la altura de la "
+                           + (i + 1) + "ª persona (en cm):");
+                   
+                        alturas[i] = entry.nextInt();
+                   
+                        if(alturas[i] < 0){//Si sale la altura negativa, la 
+                            //pasamos a positiva
+                       
+                            alturas[i] = alturas[i] * (-1);
+                       
+                        }
+                        
+                        //Añadimos la altura solicitada al total
+                        total = total + alturas[i];
+                   
+                    }
+                    
+                    //Calculamos la media a raíz del total de alturas y el nº de
+                    //las mismas...
+                    media = total / limite;
+                    //...y la mostramos por pantalla
+                    System.out.println("La altura media es de " + media + "cm");
+                    
+                    for(i = 0; i < limite; i++){//Podemos reutilizar i aquí
+                        //Bucle para comprobar cuántas personar son más altas o
+                        //más cortas que la media
+                        
+                        if(alturas[i] > media){//Si la altura supera a la media
+                            
+                            sup++;
+                            
+                        }else if(alturas[i] < media){//Si la altura no supera
+                            //a la media
+                            
+                            inf++;
+                            
+                        }
+                        
+                    }
+                    
+                    //Mostramos la cantidad de gente que es más alta/corta que 
+                    //la media:
+                    System.out.println("Personas con altura superior a la media:"
+                            + " " + sup);
+                    
+                    System.out.println("Personas con altura inferior a la media:"
+                            + " " + inf);
+                    
+                    System.out.println("Personas en la media de altura: " +
+                            (i - sup - inf));
+            
+                }else{//Sí el nº de personas es 0 o menor, da error...
+                
+                    System.out.println("Introduce un nº mayor que 0");
+                    System.out.println("PERMÍTEME QUE INSISTA");
+                
+            }
+            
+            }catch (InputMismatchException ime){//Si se introduce un caracter
+                //no numérico, salta error...
+                
+                System.out.println("ERROR. Introduce un nº");
+                System.out.println("PERMÍTEME QUE INSISTA");
+                limite = -1;
+                entry.next();
+                
+            }
+                                           
+        }while(limite <= 0);//El bucle tan querido.
         
     }
     
