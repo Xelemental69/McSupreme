79119ca8dbd792c73de61067cbf97d0264565a42
Xelemental <xelemental864@gmail.com>
2022-01-26 14:08:37 +0100

    ...

--- p42_FJMG/src/main/java/daw1st_p42_FJMG/PruebaTarjeta.java
+++ p42_FJMG/src/main/java/daw1st_p42_FJMG/PruebaTarjeta.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package daw1st_t4p42_fj.p42_fjmg;
+package daw1st_p42_FJMG;
 
 /**
  *
