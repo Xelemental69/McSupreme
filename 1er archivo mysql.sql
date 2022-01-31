f71515a42a6c02240203bbc32d056735b2bdced0
Xelemental <xelemental864@gmail.com>
2022-01-31 20:17:12 +0100

    ...

--- 1er archivo mysql.sql
+++ 1er archivo mysql.sql
@@ -1,36 +1,37 @@
 -- create database probando; Ya existe; dará error :v
 show databases;
+create database if not exists probando;
 use probando;
 
 /*Esto es un comentario. Acuérdate para la próxima */
 
  -- Esto también es un comentario, pero solo en una línea
  
- /*create table konoodioda
+ create table if not exists konodioda
  (
  codt1 int not null,
  dest1 varchar(20) not null default 'WRYYYYYYYYYYY',
- constraint pk_konodiooda primary key (codt1)
+ constraint pk_konodioda primary key (codt1)
  ); 
  
- create table konodiegoda
+ create table if not exists konodiegoda
  (
  codt2 int not null,
  dest1 varchar(20) not null default 'WRRRYYYYYYYYYYY',
  codt1 int null,
  constraint pk_konodiegoda primary key (codt2),
- constraint fk_konodiegoda_konodiooda foreign key (codt1) references konodiooda (codt1)
+ constraint fk_konodiegoda_konodioda foreign key (codt1) references konodioda (codt1)
     on delete no action on update cascade
- ); */
+ ); 
  
- create table cars
+ create table if not exists cars
  (
  codt3 int not null,
  dest3 varchar(20) not null default 'LIGHTO MODO',
  codt4 int not null,
  constraint pk_kars primary key (codt3),
- constraint fk_kars_konodiegoda foreign key (codt4) references konodiooda (codt1)
+ constraint fk_kars_konodiegoda foreign key (codt4) references konodiegoda (codt1)
     on delete no action on update cascade,
- constraint fk_kars_konodiooda foreign key (codt3) references konodiegoda (codt1)
+ constraint fk_kars_konodioda foreign key (codt3) references konodioda (codt1)
     on delete no action on update cascade
  );
