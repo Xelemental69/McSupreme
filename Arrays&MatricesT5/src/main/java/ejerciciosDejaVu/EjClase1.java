4b08f86688932853692599ea09ad768642213bd5
Xelemental <xelemental864@gmail.com>
2022-02-22 13:45:20 +0100

    Terminado (ahora recorre vecinas con un for)

--- Arrays&MatricesT5/src/main/java/ejerciciosDejaVu/EjClase1.java
+++ Arrays&MatricesT5/src/main/java/ejerciciosDejaVu/EjClase1.java
@@ -97,15 +97,16 @@ public class EjClase1 {
         for (int m = x - 1; m <= (x + 1); m++) {
 
             for (int n = y - 1; n <= (y + 1); n++) {
+
                 if (m >= 0 && n >= 0 && m < matrix.length
                         && n < matrix[0].length && !(m == x && n == y)) {
 
                     System.out.printf(matrix[m][n] + " ");
 
-                }else if (m == x && n == y){
-                    
+                } else if (m == x && n == y) {
+
                     System.out.printf("   ");
-                    
+
                 }
 
             }
