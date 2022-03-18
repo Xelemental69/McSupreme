24455fd567ef7d06dbd7b3dd1b03f86ff6bff443
Xelemental <xelemental864@gmail.com>
2022-03-18 13:38:58 +0100

    Ej. 12 POO-T6 (Falta el Main)

---
+++ T6POO/src/main/java/daw_fjmg/t6poo/CuentaAhorro.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw_fjmg.t6poo;
+
+/**
+ *
+ * @author fco-j
+ */
+public class CuentaAhorro extends Cuenta{
+    
+    private double interes;
+    private double comisionAnual;
+
+    public CuentaAhorro(Cliente cliente, double interes, double comisionAnual) {
+        
+        super(cliente);
+        this.interes = interes;
+        this.comisionAnual = comisionAnual;
+        
+    }
+
+    @Override
+    public void actualizarSaldo() {
+        
+        saldo = saldo + (saldo * interes / 100) - comisionAnual;
+        
+    }
+
+    @Override
+    public void retirar(double entry) {
+        
+        if (saldo >= entry){
+            
+            saldo -= entry;
+            
+        }else{
+            
+            System.out.println("Error. No se puede retirar tanto dinero");
+            
+        }
+    
+    }
+
+    public double getInteres() {
+        
+        return interes;
+    
+    }
+
+    public void setInteres(double interes) {
+        
+        this.interes = interes;
+    
+    }
+
+    public double getComisionAnual() {
+        
+        return comisionAnual;
+        
+    }
+
+    public void setComisionAnual(double comisionAnual) {
+        
+        this.comisionAnual = comisionAnual;
+    
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "CuentaAhorro{" + "\nInteres: " + interes + "\nComisión Anual: "
+                + comisionAnual + '}';
+    }
+    
+    
+    
+}
