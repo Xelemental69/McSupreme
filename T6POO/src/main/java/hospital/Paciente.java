74d75db1896b74cf2a84dc14c8b5b5b6ee9be67d
Xelemental <xelemental864@gmail.com>
2022-03-24 13:42:12 +0100

    Tarea Hospital (POR TERMINAR)

---
+++ T6POO/src/main/java/hospital/Paciente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospital;
+
+/**
+ *
+ * @author fco-j
+ */
+public class Paciente extends Persona{
+    
+    
+    
+}
