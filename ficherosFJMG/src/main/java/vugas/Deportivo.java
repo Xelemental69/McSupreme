8c70c3f83f3acac83c06367789ddaee13dd0f7de
Xelemental69 <xelemental864@gmail.com>
2022-04-19 14:21:39 +0200

    Para el ejercicio 4

---
+++ ficherosFJMG/src/main/java/vugas/Deportivo.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw_fjmg.t6poo;
+
+/**
+ *
+ * @author fco-j
+ */
+public class Deportivo extends Vehiculo {
+
+    private int cilindrada; // Atributo específico de Deportivo
+
+    public Deportivo(int cilindrada, Long bastidor, String matricula,
+            String marca, String modelo, String color, double tarifa,
+            boolean disponible) {
+        
+        super(bastidor, matricula, marca, modelo, color, tarifa, disponible);
+        
+        this.cilindrada = cilindrada;
+        
+    }
+    
+    public Deportivo(){
+        
+        super();
+        this.cilindrada = 4;
+        
+    }
+
+    public int getCilindrada() {
+        
+        return cilindrada;
+        
+    }
+
+    public void setCilindrada(int cilindrada) {
+        
+        this.cilindrada = cilindrada;
+        
+    }
+
+    @Override
+    public String getAtributos() {
+        
+        return super.getAtributos() + "\n\tDeportivo{" + "\n\tCilindrada: "
+                + cilindrada + '}';
+        
+    }
+
+    public void metodoDeportivo(){
+        
+        System.out.println("Este método es de la clase Deportivo");
+        
+    }
+    
+}
