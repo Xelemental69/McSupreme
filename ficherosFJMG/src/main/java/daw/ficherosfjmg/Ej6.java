854e3f4d047cbef924d0b56e6861f62a748b9fb9
Xelemental69 <xelemental864@gmail.com>
2022-04-21 12:49:49 +0200

    Completo

--- ficherosFJMG/src/main/java/daw/ficherosfjmg/Ej6.java
+++ ficherosFJMG/src/main/java/daw/ficherosfjmg/Ej6.java
@@ -18,23 +18,24 @@ public class Ej6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         // Fichero a leer
         String idFichero = "ej3.txt";
 
         // Variables para guardar los datos que se van leyendo
-        int[] tokens = new int [75];
+        String[] tokens;
         int lineNum = 0;
         String linea;
-        final int MAX = 75;//Máximo de líneas del archivo
+        int posAct = 0;
+        int posLinea = 0;
 
         System.out.println("Leyendo el fichero: " + idFichero);
-        
-     // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
+
+        // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
         // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
         // las operaciones con el archivo
-        try (Scanner datosFichero = new Scanner(new FileReader(idFichero))){
-            
+        try ( Scanner datosFichero = new Scanner(new FileReader(idFichero))) {
+
             // Mientras haya líneas por leer
             while (datosFichero.hasNextLine()) {
 
@@ -45,10 +46,10 @@ public class Ej6 {
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-        
-        try (Scanner datosFichero = new Scanner(new FileReader(idFichero))){
 
-            System.out.println("Separando cada elemento del fichero: ");
+        try ( Scanner datosFichero = new Scanner(new FileReader(idFichero))) {
+
+            System.out.println("Buscando la cadena \"web\": ");
 
             while (datosFichero.hasNextLine()) {
 
@@ -56,22 +57,71 @@ public class Ej6 {
 
                 // Se guarda en el array de String cada elemento de la
                 // línea en función del carácter separador coma
-                //tokens = linea.split(",");
+                tokens = linea.split(";");
 
-                for (int i = 0; i < MAX; i++) {
-                    
-                    System.out.print(i + "-> Posición nº " + tokens[i]);
-                    
+                for (String string : tokens) {
+
+                    if (string.equalsIgnoreCase("b")
+                            && tokens[(posAct - 1)].equalsIgnoreCase("e")
+                            && tokens[(posAct - 2)].equalsIgnoreCase("w")) {
+
+                        System.out.println("\"web\" encontrado (Posición "
+                                + (posAct - 2) + " en la Linea " + posLinea + ')');
+
+                    }
+
+                    posAct++;
+
                 }
 
-                System.out.println();
+                posAct = 0;
+                posLinea++;
+
             }
-            
+
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
-        } 
-
+        }
         
+        try ( Scanner datosFichero = new Scanner(new FileReader(idFichero))) {
+
+            System.out.println("Buscando la cadena \"DIO\": ");
+            posLinea = 0;
+
+            while (datosFichero.hasNextLine()) {
+
+                linea = datosFichero.nextLine();
+
+                // Se guarda en el array de String cada elemento de la
+                // línea en función del carácter separador punto y coma
+                tokens = linea.split(";");
+
+                for (posAct = 0; posAct < tokens.length; posAct++) {
+
+                    if (tokens[posAct].equalsIgnoreCase("d")
+                            && tokens[(posAct + 1)].equalsIgnoreCase("i")
+                            && tokens[(posAct + 2)].equalsIgnoreCase("o")) {
+
+                        System.out.println("\"DIO\" encontrado (Posición "
+                                + posAct + " en la Linea " + posLinea + ')'
+                                +   "\n\tWRYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+
+                    }
+
+                }
+
+                posLinea++;
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
-    
+
 }
+
+/*
+
+*/
