ceba5342de6fa21f9159ea830afc7a9fd740c54d
Xelemental69 <xelemental864@gmail.com>
2022-04-22 13:01:42 +0200

    Completado y comentado

--- tarea7A/src/main/java/fjmg/tarea7a/ProfFicheroCSV.java
+++ tarea7A/src/main/java/fjmg/tarea7a/ProfFicheroCSV.java
@@ -7,28 +7,32 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 /**
  *
  * @author JCarlos
  *
  */
-public class LeerFicheroCSV {
+public class ProfFicheroCSV {
 
     public static void main(String[] args) {
 
         // Fichero a leer con datos de ejemplo
         String idFichero = "RelPerCen.csv";
         String idFicheroNew = "profesoresPorDepartamento.csv";
+        String idFichero100plus = "profesoresmasde100dias.csv";
 
         // Variables para guardar los datos que se van leyendo
         String[] tokens;
         String linea;
         SortedMap<String, Profesor> profesores = new TreeMap<>();
-        //ArrayList <Profesor> profesores = new ArrayList<>();
-        Profesor plantilla = new Profesor();
-        String[] aux;
-        int contador = 0;
+        ArrayList <Profesor> profesores100plus = new ArrayList<>();
+        Profesor plantilla = new Profesor();//Lo usaremos para pasar los datos
+        int contador = 0;//Nos ayudará a pasar los datos
+        int tmp = 0;//Nos ayudará a ordenar los datos del Map
+        LocalDate fecini, fecfin;//Paa la comparación de días
         
 
         System.out.println("Leyendo el fichero: " + idFichero);
@@ -55,47 +59,58 @@ public class LeerFicheroCSV {
         
         try (Scanner datosFichero = new Scanner(new File(idFichero), "ISO-8859-3")) {
             // hasNextLine devuelve true mientras haya líneas por leer
-            datosFichero.nextLine();
+            if(datosFichero.hasNextLine()) {
+                datosFichero.nextLine();//Saltamos la primera línea:
+                System.out.println("Leyendo el fichero hacia el map...");
+            }
             while (datosFichero.hasNextLine()) {
                 // Guarda la línea completa en un String                
                 linea = datosFichero.nextLine();
                 // Se guarda en el array de String cada elemento de la
                 // línea en función del carácter separador de campos del fichero CSV
-                tokens = linea.split(",");
+                tokens = linea.split(",");//Cáracter separador
                 for (String string : tokens) {
-                    
-                    switch(contador){
+                    //System.out.println("contador: " + contador);
+                    switch(contador){//Dependiendo del valor de contador, se añadirá
+                        //el contenido de string en el atributo correspondiente:
                         
                         case 0:
                             plantilla.setNombre(string);
+                            //System.out.println("nombre recibido");
                             break;
                         
                         case 1:
                             plantilla.setApellidos(string);
+                            //System.out.println("apellido recibido");
                             break;
                         
                         case 2:
                             plantilla.setDni(string);
+                            //System.out.println("dni recibido");
                             break;
                         
                         case 3:
                             plantilla.setPuesto(string);
+                            //System.out.println("puesto recibido");
                             break;
                         
                         case 4:
                             plantilla.setFechaIni(string);
+                            //System.out.println("fecini recibido");
                             break;
                             
                         case 5:
                             plantilla.setFechaCese(string);
+                            //System.out.println("fecfin recibido");
                             break;
                             
                         case 6:
                             plantilla.setTelefono(string);
+                            //System.out.println("telefono recibido");
                             break;
                             
                         case 7:
-                            if(string.equals("Sí")){
+                            if(string.equals("\"Sí\"")){//Recoge el sí junto a sus comillas
                                 
                                 plantilla.setEvaluador(true);
                                 
@@ -104,35 +119,119 @@ public class LeerFicheroCSV {
                                 plantilla.setEvaluador(false);
                                 
                             }
+                            //System.out.println("eval recibido");
+                            break;
+                            
+                        case 8:
+                            if(string.equals("\"Sí\"")){//Recoge el sí junto a sus comillas
+                                
+                                plantilla.setCoordinador(true);
+                                
+                            }else{
+                                
+                                plantilla.setCoordinador(false);
+                                
+                            }
+                            //System.out.println("coord recibido");
                             break;
                         
                     }
                     
                     contador++;
-                    
-                    System.out.print(string + "\t");
+                    tmp++;
                 }                
-                profesores.put(plantilla.getDni(), plantilla);
+                profesores.put(plantilla.getPuesto() + tmp, plantilla);
                 plantilla = new Profesor();
+                contador = 0;
             }
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
                 
-        //Collections.sort(profesores, (Profesor p1,Profesor p2)->p1.getDni().compareTo(p2.getDni()));
+        for(String key : profesores.keySet()){
+                System.out.println(profesores.get(key).toString());
+            }
+        
+
         
         try ( BufferedWriter flujo = new BufferedWriter(new FileWriter(idFicheroNew))) {
 
-            flujo.write("Empleado/a\",\"DNI/Pasaporte\",\"Puesto\",\"Fecha de toma de posesión\",\"Fecha de cese\",\"Teléfono\",\"Evaluador\",\"Coordinador");
+            System.out.println("Generando el fichero...");
+            //Imprimimos las categorías primero:
+            flujo.write("Empleado/a\tDNI/Pasaporte\tPuesto\tFecha de toma de posesión\tFecha de cese\tTeléfono\tEvaluador\tCoordinador");
             flujo.newLine();
-            
-//            profesores.forEach((clave, valor) -> System.out.println("Clave " +
-//                clave + " -- Objeto " + valor));
-            
+                        
             for(String key : profesores.keySet()){
                                                
-                //Escribe el valor de linea:
+                //Escribe el valor del elemento del map (su toString()):
                 flujo.write(profesores.get(key).toString());
+                // Metodo newLine() añade línea en blanco
+                flujo.newLine();
+                
+            }
+            // Metodo flush() guarda cambios en disco 
+            flujo.flush();
+
+        } catch (IOException e) {
+            
+            System.out.println(e.getMessage());
+            
+        }
+        
+        //Saca y comprueba las fechas (100+ días):
+        for(String key : profesores.keySet()){
+                
+            //Mete los strings en tokens, separándolos por el caracter "/":           
+            tokens=profesores.get(key).getFechaIni().split("/");
+
+            //Para poder usarlo como fecha inicial, quitamos las comillas de los strings:
+            fecini = LocalDate.of(Integer.parseInt(tokens[2].replace("\"", "")),
+                    Integer.parseInt(tokens[1]),
+                    Integer.parseInt(tokens[0].replace("\"", "")));
+
+            //Procederá a recoger la fecha si contiene el separador
+            //(en caso contrario, es que está vacío):
+            if(profesores.get(key).getFechaCese().contains("/")){
+                
+                //Mete los strings en tokens, separándolos por el caracter "/":
+                tokens=profesores.get(key).getFechaCese().split("/");
+                
+                //Para poder usarlo como fecha inicial, quitamos las comillas de los strings:
+                fecfin = LocalDate.of(Integer.parseInt(tokens[2].replace("\"", "")),
+                        Integer.parseInt(tokens[1]),
+                        Integer.parseInt(tokens[0].replace("\"", "")));
+
+                    //En caso de que la fecha de cese no esté en vacío, comprobamos si la
+                    //diferencia entre ambas fechas es de más de 100 días:
+                    if(ChronoUnit.DAYS.between(fecini, fecfin) > 100){
+
+                        //Si se cumple la condición, agregamos la clase al ArrayList "profesores100plus"
+                        profesores100plus.add(profesores.get(key));
+
+                    }
+
+            }
+
+            for(Profesor p : profesores100plus){
+
+                System.out.println(p.toString());
+
+            }
+                
+        }
+        
+        //Imprimimos el ArrayList en un fichero nuevo, con cada línea por clase y usando el toString:
+        try ( BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero100plus))) {
+
+            System.out.println("Generando el fichero...");
+            //Imprimimos las categorías primero:
+            flujo.write("Empleado/a\tDNI/Pasaporte\tPuesto\tFecha de toma de posesión\tFecha de cese\tTeléfono\tEvaluador\tCoordinador");
+            flujo.newLine();
+                        
+            for(Profesor p : profesores100plus){
+                                               
+                //Escribe el valor del elemento del map (su toString()):
+                flujo.write(p.toString());
                 // Metodo newLine() añade línea en blanco
                 flujo.newLine();
                 
